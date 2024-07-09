a9522d0f8a15e2a6b2b60e06e2df927de5e54506
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-07-09 13:57:03 +0800

    Added routes by closure

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+# Route define using Closure method
+Route::get('/hi', function () {
+    return '<h1>hi</h1>';
+});
+
+Route::get('/hi-from-view', function () {
+    return view('hi.from-view');
+});
+
+Route::get('/hi-konchiwa', function () {
+    return view('hi.nihongo.konichiwa');
+});
+
