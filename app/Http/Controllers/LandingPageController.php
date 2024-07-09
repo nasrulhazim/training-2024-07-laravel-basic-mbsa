456315f38c56c5899d16dfb97b37b3bbbf8ee168
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-07-09 14:31:29 +0800

    Setup route using controllers

---
+++ app/Http/Controllers/LandingPageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LandingPageController extends Controller
+{
+    public function myBayar()
+    {
+        return 'myBayar';
+    }
+
+    public function eLoyalty()
+    {
+        return 'eLoyalty';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,32 @@
 <?php
 
+use App\Http\Controllers\LandingPageController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 # Route define using Closure method
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+# Route by using Controller (invokable)
+Route::get('/', WelcomeController::class);
+
+# Route by using Controller (using custom method)
+Route::get('/my-bayar', [
+    LandingPageController::class,
+    'myBayar'
+]);
+
+Route::get('/e-loyalty', [
+    LandingPageController::class,
+    'eLoyalty'
+]);
+
+Route::resource('users', UserController::class);
+
+
 Route::get('/hi', function () {
     return '<h1>hi</h1>';
 });
