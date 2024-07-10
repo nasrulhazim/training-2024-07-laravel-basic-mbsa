c27d46c11f85944a6a5ad6ce06097bb1d60d9eae
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-07-10 11:23:23 +0800

    Apply new layout with Laravel Jetstream Layout

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,6 +1,27 @@
-<ol>
-    {{-- {{ $users->links() }} --}}
-    @foreach ($users as $user)
-        <li>{{ $user->name }}</li>
-    @endforeach
-</ol>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Profile') }}
+        </h2>
+    </x-slot>
+
+
+    <div>
+        <div class="bg-white max-w-7xl mt-10 mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="mt-5 md:mt-0 md:col-span-2">
+                <div class="flex justify-end">
+                    {{ $users->links() }}
+                </div>
+                <ol>
+
+                    @foreach ($users as $user)
+                        <li>{{ $user->name }}</li>
+                    @endforeach
+                </ol>
+                <div class="flex justify-end">
+                    {{ $users->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
