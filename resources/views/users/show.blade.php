6b56960a604832e875dab7e5317c2fc41bee17e8
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-07-10 15:39:28 +0800

    Added back button in show details

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,10 +8,13 @@
     <div>
         <div class="bg-white max-w-7xl mt-10 mx-auto py-10 sm:px-6 lg:px-8">
             <div class="mt-5 md:mt-0 md:col-span-2">
-                <ol>
+                <ol class="my-4">
                     <li>{{ $user->name }}</li>
                     <li>{{ $user->email }}</li>
                 </ol>
+
+                <a class="text-sm  hover:bg-gray-100 rounded-md  px-4 py-2  border-slate-300 border-2"
+                    href="{{ route('users.index') }}">Back</a>
             </div>
         </div>
     </div>
