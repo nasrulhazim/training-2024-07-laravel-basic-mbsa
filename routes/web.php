d2f7d3a3888f74e5376f57ded2818d43642214d4
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-07-10 12:20:43 +0800

    Move route for UserController in Auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
@@ -13,33 +12,6 @@ use Illuminate\Support\Facades\Route;
 # Route by using Controller (invokable)
 Route::get('/', WelcomeController::class);
 
-# Route by using Controller (using custom method)
-Route::get('/my-bayar', [
-    LandingPageController::class,
-    'myBayar'
-]);
-
-Route::get('/e-loyalty', [
-    LandingPageController::class,
-    'eLoyalty'
-]);
-
-Route::resource('users', UserController::class);
-
-
-Route::get('/hi', function () {
-    return '<h1>hi</h1>';
-});
-
-Route::get('/hi-from-view', function () {
-    return view('hi.from-view');
-});
-
-Route::get('/hi-konchiwa', function () {
-    return view('hi.nihongo.konichiwa');
-});
-
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -48,4 +20,6 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::resource('users', UserController::class);
 });
